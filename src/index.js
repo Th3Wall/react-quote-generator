571d956307653c6eed477917207dd5a3d7daee46
Th3Wall <mandellidavide.95@gmail.com>
2020-12-18 02:58:37 +0100

    fixed some height issues & reduced floating btn width/height in mobile

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+const appHeight = () => document.documentElement.style.setProperty('--vh', `${window.innerHeight}px`)
+window.addEventListener('resize', appHeight)
+appHeight();
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
